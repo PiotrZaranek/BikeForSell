80b4d12c8eaee36932f38a5b995b5d1e9b14cd8e
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-13 17:45:01 +0100

    Improved Method GetBikeForEdit GetBikeDetails.

--- BikeForSell.Infrastructure/Repositories/BikeRepository.cs
+++ BikeForSell.Infrastructure/Repositories/BikeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BikeForSell.Domain.Interfaces;
 using BikeForSell.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,6 @@ namespace BikeForSell.Infrastructure.Repositories
         public IQueryable<Bike> GetAllActiveBikes()
         {
             var bikes = _context.Bikes.Where(x => x.IsActive == true).Where(x => x.IsBought == false);
-
             return bikes;
         }
 
@@ -37,18 +37,13 @@ namespace BikeForSell.Infrastructure.Repositories
         
         public Bike GetBikeDetails(int id)
         {
-            var bike = _context.Bikes.FirstOrDefault(x => x.Id == id);
-
-            if (bike == null)
-            {
-                return bike;
-            }
-
-            bike.Frame = _context.Frames.FirstOrDefault(x => x.BikeRef == id);
-            bike.DetailInformation = _context.DetailInformations.FirstOrDefault(x => x.BikeRef == id);
-            bike.Drive = _context.Drives.FirstOrDefault(x => x.BikeRef == id);
-            bike.Brake = _context.Brakes.FirstOrDefault(x => x.BikeRef == id);
-            bike.Wheel = _context.Wheels.FirstOrDefault(x => x.BikeRef == id);      
+            var bike = _context.Bikes
+                .Include(d => d.DetailInformation)
+                .Include(f => f.Frame)
+                .Include(d => d.Drive)
+                .Include(b => b.Brake)
+                .Include(w => w.Wheel)
+                .FirstOrDefault(k => k.Id == id);     
             
             return bike;
         }
@@ -80,20 +75,15 @@ namespace BikeForSell.Infrastructure.Repositories
             }            
         }
 
-        public Bike GetBikeforEdit(int id)
+        public Bike GetBikeForEdit(int id)
         {
-            var bike = _context.Bikes.FirstOrDefault(x => x.Id == id);
-
-            if (bike == null)
-            {
-                return bike;
-            }
-
-            bike.Frame = _context.Frames.FirstOrDefault(x => x.BikeRef == id);
-            bike.DetailInformation = _context.DetailInformations.FirstOrDefault(x => x.BikeRef == id);
-            bike.Drive = _context.Drives.FirstOrDefault(x => x.BikeRef == id);
-            bike.Brake = _context.Brakes.FirstOrDefault(x => x.BikeRef == id);
-            bike.Wheel = _context.Wheels.FirstOrDefault(x => x.BikeRef == id);
+            var bike = _context.Bikes
+                .Include(d => d.DetailInformation)
+                .Include(f => f.Frame)
+                .Include(d => d.Drive)
+                .Include(b => b.Brake)
+                .Include(w => w.Wheel)
+                .FirstOrDefault(k => k.Id == id);
 
             return bike;
         }
