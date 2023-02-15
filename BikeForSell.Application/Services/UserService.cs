88b978de07e5aa4abd4c6eb45fcabf81e0cdcc03
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-15 19:50:22 +0100

    Added class UserService.

---
+++ BikeForSell.Application/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using BikeForSell.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeForSell.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _context;
+
+        public UserService(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+
+        public string GetUserId()
+        {
+            var id = _context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+            return id.Value;
+        }
+    }
+}
