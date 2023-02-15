01ef4142e22cc02adad836afb8e005e396760684
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-15 19:53:24 +0100

    Corrected type of Id from int to string. Deleted id parameter in BuyBike method.

--- BikeForSell.Domain/Interfaces/IBikeRepository.cs
+++ BikeForSell.Domain/Interfaces/IBikeRepository.cs
@@ -7,11 +7,11 @@ namespace BikeForSell.Domain.Interfaces
         IQueryable<Bike> GetAllActiveBikes();
         int Add(Bike bike);
         Bike GetBikeDetails(int id);
-        IQueryable GetYourBikesList(int id);
+        IQueryable GetYourBikesList(string id);
         void ChangeStatus(int id);
         Bike GetBikeForEdit(int id);
         void EditBike(Bike bike);
         void DeleteBike(int id);
-        void BuyBike(Transaction transaction);
+        void BuyBike(Transaction transaction, int id);
     }
 }
