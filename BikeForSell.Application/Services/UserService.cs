e271132c2fccfadf3ac4743ee4333360b8e58508
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-20 14:56:33 +0100

    Added inject to SignInManager. Added LogOutUser() method.

--- BikeForSell.Application/Services/UserService.cs
+++ BikeForSell.Application/Services/UserService.cs
@@ -1,27 +1,32 @@
 ﻿using BikeForSell.Application.Interfaces;
+using BikeForSell.Domain.Interfaces;
+using BikeForSell.Domain.Models;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BikeForSell.Application.Services
 {
     public class UserService : IUserService
     {
         private readonly IHttpContextAccessor _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public UserService(IHttpContextAccessor context)
+        public UserService(IHttpContextAccessor context, SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _signInManager = signInManager;
         }
 
         public string GetUserId()
         {
             var id = _context.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
             return id.Value;
+        }
+
+        public async void LogOutUser()
+        {
+            await _signInManager.SignOutAsync();
         }
     }
 }
