f671e9888e7a855643de86eb4eea4196d24d0798
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-20 15:16:33 +0100

    Added authorize to actions Added method to control user.

--- BikeForSell.Web/Controllers/ProfileController.cs
+++ BikeForSell.Web/Controllers/ProfileController.cs
@@ -1,12 +1,15 @@
-﻿using BikeForSell.Application.Interfaces;
+﻿using AutoMapper.Configuration.Conventions;
+using BikeForSell.Application.Interfaces;
 using BikeForSell.Application.Services;
+using BikeForSell.Application.ViewModels.ProfileVm;
+using BikeForSell.Domain.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Runtime.CompilerServices;
 
 namespace BikeForSell.Web.Controllers
 {
-    [Authorize]
     public class ProfileController : Controller
     {
         private readonly IProfileService _profileService;
@@ -19,12 +22,14 @@ namespace BikeForSell.Web.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Allowed")]
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet]
+        [Authorize(Roles = "Allowed")]
         public IActionResult Purchase()
         {
             string id = _userService.GetUserId();
@@ -32,6 +37,7 @@ namespace BikeForSell.Web.Controllers
             return View(model);
         }
 
+        [Authorize(Roles = "Allowed")]
         public IActionResult DeletePurchase(int id)
         {
             _profileService.DeletePurchase(id);
@@ -39,6 +45,7 @@ namespace BikeForSell.Web.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "Allowed")]
         public IActionResult Sales()
         {
             string id = _userService.GetUserId();
@@ -46,16 +53,89 @@ namespace BikeForSell.Web.Controllers
             return View(model);
         }
 
+        [Authorize(Roles = "Allowed")]
         public IActionResult ChangeState(int saleId, int decision)
         {
             _profileService.ChangeState(saleId, decision);
             return RedirectToAction("Sales");
         }
 
+        [Authorize(Roles = "Allowed")]
         public IActionResult DeleteSale(int id)
         {
             _profileService.DeleteSale(id);
             return RedirectToAction("Sales");
+        }
+
+        [Authorize(Roles = "NotAllowed, Allowed")]        
+        public IActionResult CheckUserDetalInformation()
+        {
+            string id = _userService.GetUserId();
+
+            if (_profileService.UserDetalInformation(id))
+            {
+                return RedirectToAction("Index", "Bike");
+            }
+            else
+            {
+                return RedirectToAction("AddDetalInformation");
+            }            
+        }
+
+        [HttpGet]
+        [Authorize( Roles = "NotAllowed")]
+        public IActionResult AddDetalInformation()
+        {
+            var model = new DetalInformationVm();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddDetalInformation(DetalInformationVm model)
+        {
+            if(ModelState.IsValid)
+            {
+                model.Id = _userService.GetUserId();
+                _profileService.AddDetalInformation(model);             
+                return RedirectToAction("SuccessfullyAddedInformation");
+            }
+            else
+            {
+                return View(model);
+            }            
+        }
+
+        [Authorize(Roles = "NotAllowed")]
+        public IActionResult SuccessfullyAddedInformation()
+        {
+            _userService.LogOutUser();
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Allowed")]
+        public IActionResult EditDetalInformation()
+        {
+            var model = new EditDetalInformationVm();
+            model.Id = _userService.GetUserId();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Allowed")]
+        public IActionResult EditDetalInformation(EditDetalInformationVm model)
+        {
+            if (ModelState.IsValid)
+            {
+                _profileService.EditDetalInformation(model);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(model);
+            }            
         }
     }
 }
