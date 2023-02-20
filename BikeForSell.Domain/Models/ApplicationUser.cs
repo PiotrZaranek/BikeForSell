935e2d7043e5acf7dd70eac2188429b2aa91f0f4
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-20 14:58:07 +0100

    Added ApplicationUser model.

---
+++ BikeForSell.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BikeForSell.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirsName { get; set; }
+        public string? LastName { get; set; }
+        public bool AddedDetalInformation { get; set; }
+
+        // 1:1 DetalInformation to ApplicationUser
+        public DetailInformation DetailInformation { get; set; }
+    }
+}
