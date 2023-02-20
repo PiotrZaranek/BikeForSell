82622de0d83b0f2307382e291881a06088b6543b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-20 14:53:09 +0100

    Added LogOutUser() method.

--- BikeForSell.Application/Interfaces/IUserService.cs
+++ BikeForSell.Application/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper.Configuration.Conventions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace BikeForSell.Application.Interfaces
     public interface IUserService
     {
         string GetUserId();
+        void LogOutUser();
     }
 }
