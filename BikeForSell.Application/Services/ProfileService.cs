206cb976f8dd52a0a72e7d1043735d415b7e7cb9
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-02-20 14:54:36 +0100

    Added methods to control user.

--- BikeForSell.Application/Services/ProfileService.cs
+++ BikeForSell.Application/Services/ProfileService.cs
@@ -4,8 +4,11 @@ using AutoMapper.QueryableExtensions;
 using BikeForSell.Application.Interfaces;
 using BikeForSell.Application.ViewModels.ProfileVm;
 using BikeForSell.Domain.Interfaces;
+using BikeForSell.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,8 +56,6 @@ namespace BikeForSell.Application.Services
                 Size = sales.Count
             };
 
-            salesVm.Sales.OrderBy(x => x.Date);
-
             return salesVm;
         }
 
@@ -66,6 +67,41 @@ namespace BikeForSell.Application.Services
         public void DeleteSale(int id)
         {
             _profileRepo.DeleteSale(id);
+        }
+
+        public ApplicationUser GetUser(string id)
+        {
+            return _profileRepo.GetUser(id);
+        }
+
+        public bool UserDetalInformation(string id)
+        {
+            return _profileRepo.UserDetalInformation(id);
+        }
+
+        public void AddDetalInformation(DetalInformationVm model)
+        {
+            var user = _profileRepo.GetUser(model.Id);
+            user.PhoneNumber = model.PhoneNumber;
+            user.FirsName = model.FirsName;
+            user.LastName = model.LastName;
+            user.AddedDetalInformation = true;
+
+            var role = new IdentityUserRole<string>();
+            role.UserId = user.Id;
+            role.RoleId = "Allowed";
+
+            _profileRepo.AddDetalInfroamtion(user, role);
+        }
+
+        public void EditDetalInformation(EditDetalInformationVm model)
+        {
+            var user = _profileRepo.GetUser(model.Id);
+            user.FirsName = model.FirsName;
+            user.LastName = model.LastName;
+            user.PhoneNumber = model.PhoneNumber;
+
+            _profileRepo.EditDetalInformation(user);
         }
     }
 }
