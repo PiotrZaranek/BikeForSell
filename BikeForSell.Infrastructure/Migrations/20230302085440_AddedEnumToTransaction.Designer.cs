3d21429c7ad54a91dfe01f163b4d00f44ed73522
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-03-02 10:48:11 +0100

    Added migration.

--- BikeForSell.Infrastructure/Migrations/20230302085440_AddedEnumToTransaction.Designer.cs
+++ BikeForSell.Infrastructure/Migrations/20230302085440_AddedEnumToTransaction.Designer.cs
@@ -4,6 +4,7 @@ using BikeForSell.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BikeForSell.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230302085440_AddedEnumToTransaction")]
+    partial class AddedEnumToTransaction
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
