614d1fb866c4e2c7c15d363f57f220cdada6b05b
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-03-02 10:47:48 +0100

    Deleted nullable property in BuyerId, SalemanId Changed type of  State property.

--- BikeForSell.Domain/Models/Transaction.cs
+++ BikeForSell.Domain/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BikeForSell.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,10 @@ namespace BikeForSell.Domain.Models
     public class Transaction
     {
         public int Id { get; set; }
-        public string? BuyerId { get; set; }
-        public string? SalemanId { get; set; }
+        public string BuyerId { get; set; }
+        public string SalemanId { get; set; }
         public DateTime Date { get; set; }
-        public int State { get; set; }
+        public State State { get; set; }
 
         // 1:1 Bike To Transaction
         public int BikeRef { get; set; }
