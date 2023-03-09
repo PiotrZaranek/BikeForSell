5e30c8f6304c54a7a7cf28589e1126e953a5fd06
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-03-09 12:46:01 +0100

    Created new private methods and refactored public methods.

--- BikeForSell.Application/Services/BikeService.cs
+++ BikeForSell.Application/Services/BikeService.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
 using AutoMapper.Configuration.Conventions;
+using System.Runtime.InteropServices;
 
 namespace BikeForSell.Application.Services
 {
@@ -28,13 +29,8 @@ namespace BikeForSell.Application.Services
         }
 
         public int AddBike(NewBikeVm bikeVm, ApplicationUser user)
-        {
-            bikeVm.IsActive = true;
-            bikeVm.IsBought = false;
-
-            bikeVm.DetailInformation.Date = DateTime.Now;
-            bikeVm.DetailInformation.UserRef = user.Id;
-            bikeVm.DetailInformation.User = user;
+        {            
+            AssigmentProperties(bikeVm, user);
 
             var bike = _mapper.Map<Bike>(bikeVm);
             int idBike = _bikeRepo.Add(bike);
@@ -47,12 +43,7 @@ namespace BikeForSell.Application.Services
             var bikes = _bikeRepo.GetAllActiveBikes()
                 .ProjectTo<BikeForListVm>(_mapper.ConfigurationProvider).ToList();
 
-            var bikesList = new ListBikeForListVm()
-            {
-                Bikes = bikes,
-                Size = bikes.Count,
-                BikeFilterParameters = new BikeFilterParameters()
-            };
+            var bikesList = CreateListBikeForList(bikes, new BikeFilterParameters());
 
             return bikesList;
         }
@@ -63,18 +54,92 @@ namespace BikeForSell.Application.Services
                 .ProjectTo<BikeForListVm>(_mapper.ConfigurationProvider).ToList();
 
             bikes = Filtering(bikes, bikeFilter);
+            var bikesList = CreateListBikeForList(bikes, bikeFilter);
 
-            var bikesList = new ListBikeForListVm()
+            return bikesList;
+        }     
+
+        public BikeForDetailsVm GetBikeDetails(int bikeId)
+        {
+            var bike = _bikeRepo.GetBikeForDetailsOrEdit(bikeId);
+            var bikeVm = _mapper.Map<BikeForDetailsVm>(bike);
+
+            return bikeVm;
+        }
+
+        public ListBiekForYourBikes GetYourBikesList(string userId)
+        {
+            var bikes = _bikeRepo.GetYourBikesList(userId)
+                .ProjectTo<BikeForYourBikesVm>(_mapper.ConfigurationProvider).ToList();
+
+            var bikeList = CreateListBiekForYourBikes(bikes);
+
+            return bikeList;
+        }
+
+        public void ChangeStatus(int bikeId)
+        {
+            _bikeRepo.ChangeStatus(bikeId);
+        }
+
+        public BikeForEditVm GetBikeForEdit(int bikeId)
+        {
+            var bike = _bikeRepo.GetBikeForDetailsOrEdit(bikeId);
+            var bikeVm = _mapper.Map<BikeForEditVm>(bike);
+
+            return bikeVm;
+        }
+
+        public void EditBike(BikeForEditVm bikeVm)
+        {
+            var bike = _mapper.Map<Bike>(bikeVm);
+
+            _bikeRepo.EditBike(bike);
+        }
+
+        public void DeleteBike(int bikeId)
+        {
+            _bikeRepo.DeleteBike(bikeId);
+        }
+
+        public void BuyBike(int bikeId, string buyerId)
+        {
+            var Transaction = CreateTransaction(buyerId);
+
+            _bikeRepo.BuyBike(Transaction, bikeId);       
+        }
+
+
+        //Private methods
+
+        private Transaction CreateTransaction(string buyerId)
+        {
+            return new Transaction()
+            {
+                BuyerId = buyerId,
+                Date = DateTime.Now,
+                State = 0
+            };
+        }
+        private ListBiekForYourBikes CreateListBiekForYourBikes(List<BikeForYourBikesVm> bike)
+        {
+            return new ListBiekForYourBikes()
+            {
+                Bikes = bike,
+                Size = bike.Count
+            };
+        }
+        private ListBikeForListVm CreateListBikeForList(List<BikeForListVm> bikes, BikeFilterParameters bikeFilter)
+        {
+            return new ListBikeForListVm()
             {
                 Bikes = bikes,
                 Size = bikes.Count,
-                BikeFilterParameters = bikeFilter               
+                BikeFilterParameters = bikeFilter
             };
-
-            return bikesList;
         }
 
-        private static List<BikeForListVm> Filtering(List<BikeForListVm> bikes, BikeFilterParameters bikeFilters)
+        private List<BikeForListVm> Filtering(List<BikeForListVm> bikes, BikeFilterParameters bikeFilters)
         {
             if (bikeFilters.SearchString == null)
             {
@@ -95,7 +160,7 @@ namespace BikeForSell.Application.Services
                 .Where(x => x.Prize >= bikeFilters.PrizeFrom)
                 .Where(x => x.Prize <= bikeFilters.PrizeTo)
                 .Where(x => x.Type.StartsWith(bikeFilters.Type))
-                .ToList();            
+                .ToList();
 
             switch (bikeFilters.Filter)
             {
@@ -124,63 +189,14 @@ namespace BikeForSell.Application.Services
             return bikes;
         }
 
-        public BikeForDetailsVm GetBikeDetails(int bikeId)
+        private void AssigmentProperties(NewBikeVm bikeVm, ApplicationUser user)
         {
-            var bike = _bikeRepo.GetBikeDetails(bikeId);
-            var bikeVm = _mapper.Map<BikeForDetailsVm>(bike);
+            bikeVm.IsActive = true;
+            bikeVm.IsBought = false;
 
-            return bikeVm;
-        }
-
-        public ListBiekForYourBikes GetYourBikesList(string userId)
-        {
-            var bikes = _bikeRepo.GetYourBikesList(userId)
-                .ProjectTo<BikeForYourBikesVm>(_mapper.ConfigurationProvider).ToList();            
-
-            var bikeList = new ListBiekForYourBikes()
-            {
-                Bikes = bikes,
-                Size = bikes.Count
-            };
-
-            return bikeList;
-        }
-
-        public void ChangeStatus(int bikeId)
-        {
-            _bikeRepo.ChangeStatus(bikeId);
-        }
-
-        public BikeForEditVm GetBikeForEdit(int bikeId)
-        {
-            var bike = _bikeRepo.GetBikeForEdit(bikeId);
-            var bikeVm = _mapper.Map<BikeForEditVm>(bike);
-
-            return bikeVm;
-        }
-
-        public void EditBike(BikeForEditVm bikeVm)
-        {
-            var bike = _mapper.Map<Bike>(bikeVm);
-
-            _bikeRepo.EditBike(bike);
-        }
-
-        public void DeleteBike(int bikeId)
-        {
-            _bikeRepo.DeleteBike(bikeId);
-        }
-
-        public void BuyBike(int bikeId, string buyerId)
-        {
-            var Transaction = new Transaction()
-            {
-                BuyerId = buyerId,
-                Date = DateTime.Now,
-                State = 0,                
-            };
-
-            _bikeRepo.BuyBike(Transaction, bikeId);       
+            bikeVm.DetailInformation.Date = DateTime.Now;
+            bikeVm.DetailInformation.UserRef = user.Id;
+            bikeVm.DetailInformation.User = user;
         }
     }
 }
