ec50f3ac3940eaf592fa6ea2ae5e06655ad7eb38
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-04-13 13:15:00 +0200

    Added ErrorService. Added try, catch blocks in actions.

--- BikeForSell.Web/Controllers/BikeController.cs
+++ BikeForSell.Web/Controllers/BikeController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BikeForSell.Application.Interfaces;
 using BikeForSell.Application.ViewModels.BikeVm;
+using BikeForSell.Domain.Exceptions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.RegularExpressions;
@@ -13,12 +14,14 @@ namespace BikeForSell.Web.Controllers
         private readonly IBikeService _bikeService;
         private readonly IProfileService _profileService;
         private readonly IUserService _userService;
+        private readonly IErrorService _errorService;
 
-        public BikeController(IBikeService bikeService, IUserService userService, IProfileService profileService)
+        public BikeController(IBikeService bikeService, IUserService userService, IProfileService profileService, IErrorService errorService)
         {
             _bikeService = bikeService;
             _userService = userService;
             _profileService = profileService;
+            _errorService = errorService;
         }
 
         [HttpGet]
@@ -40,15 +43,37 @@ namespace BikeForSell.Web.Controllers
         [HttpGet]        
         public IActionResult DetailsForIndex(int bikeId)
         {
-            var bikeDetails = _bikeService.GetBikeDetails(bikeId);
-            bikeDetails.CurrentUserId = _userService.GetUserId();            
+            BikeForDetailsVm bikeDetails;
+
+            try
+            {
+                bikeDetails = _bikeService.GetBikeDetails(bikeId);
+                bikeDetails.CurrentUserId = _userService.GetUserId();
+            }            
+            catch (Exception ex)
+            {
+                _errorService.LogError(ex, "Bike", "DetailsForIndex", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+
             return View(bikeDetails);
         }
 
         [HttpGet]
         public IActionResult DetailsForPurchase(int bikeId)
         {
-            var bikeDetails = _bikeService.GetBikeDetails(bikeId);
+            BikeForDetailsVm bikeDetails;
+
+            try
+            {
+                bikeDetails = _bikeService.GetBikeDetails(bikeId);
+            }
+            catch (Exception ex)
+            {
+                _errorService.LogError(ex, "Bike", "DetailsForPurchase", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+            
             return View(bikeDetails);
         }
 
@@ -69,45 +94,107 @@ namespace BikeForSell.Web.Controllers
 
         [HttpPost]
         public IActionResult Add(NewBikeVm newBikeVm)
-        {
-            var user = _profileService.GetUser(_userService.GetUserId());        
-            _bikeService.AddBike(newBikeVm, user);
+        {            
+            var user = _profileService.GetUser(_userService.GetUserId());    
+            
+            try
+            {
+                _bikeService.AddBike(newBikeVm, user);
+            }
+            catch (Exception ex)
+            {
+                _errorService.LogError(ex, "Bike", "Add", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }            
 
             return RedirectToAction("YourBikes");
         }
 
         public IActionResult ChangeStatus(int bikeId)
         {
-            _bikeService.ChangeStatus(bikeId);
+            try 
+            {
+                _bikeService.ChangeStatus(bikeId);
+            }            
+            catch (Exception ex)
+            {
+                _errorService.LogError(ex, "Bike", "ChangeStatus", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+
             return RedirectToAction("YourBikes");
         }
 
         [HttpGet]
         public IActionResult Edit(int bikeId)
         {
-            var bikeEdit = _bikeService.GetBikeForEdit(bikeId);
+            BikeForEditVm bikeEdit;
+
+            try
+            {
+                bikeEdit = _bikeService.GetBikeForEdit(bikeId);
+            }
+            catch (Exception ex) 
+            {
+                _errorService.LogError(ex, "Bike", "Edit", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+            
             return View(bikeEdit);
         }
 
         [HttpPost]
         public IActionResult Edit(BikeForEditVm bike)
         {
-            _bikeService.EditBike(bike);
+            try
+            {
+                _bikeService.EditBike(bike);
+            }
+            catch (Exception ex) 
+            {
+                _errorService.LogError(ex, "Bike", "Edit", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+            
             return RedirectToAction("YourBikes");
         }
 
         public IActionResult Delete(int bikeId)
         {
-            _bikeService.DeleteBike(bikeId);
+            try
+            {
+                _bikeService.DeleteBike(bikeId);
+            }            
+            catch (Exception ex) 
+            {
+                _errorService.LogError(ex, "Bike", "Delete", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+
             return RedirectToAction("YourBikes");
         }
 
         public IActionResult Buy(int bikeId)
         {
             string userId = _userService.GetUserId(); 
-            _bikeService.BuyBike(bikeId, userId);
+
+            try
+            {
+                _bikeService.BuyBike(bikeId, userId);
+            }
+            catch (Exception ex)
+            {
+                _errorService.LogError(ex, "Bike", "Buy", _userService.GetUserId());
+                return RedirectToAction("Error");
+            }
+            
 
             return RedirectToAction("Purchase", "Profile");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
         }
     }
 }
