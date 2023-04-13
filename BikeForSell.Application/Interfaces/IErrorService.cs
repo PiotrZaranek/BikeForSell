03887f9bb7fe687bf63ae48a77d074432912c732
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-04-13 13:07:53 +0200

    Added  IErrorService.

---
+++ BikeForSell.Application/Interfaces/IErrorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeForSell.Application.Interfaces
+{
+    public interface IErrorService
+    {
+        void LogError(Exception ex, string controller, string action, string userId);
+    }
+}
