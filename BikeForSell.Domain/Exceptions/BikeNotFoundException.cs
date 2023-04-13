d6f33691f126646b0492bed8ac4ccef9865db7b7
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-04-13 13:08:48 +0200

    Added BikeNotFoundException.

---
+++ BikeForSell.Domain/Exceptions/BikeNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeForSell.Domain.Exceptions
+{
+    public class BikeNotFoundException : Exception
+    {
+        public BikeNotFoundException() : base("Bike not found in database!") { }
+    }
+}
