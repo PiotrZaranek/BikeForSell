4162890e67cde1125ba9a53309d345890a37ef53
Piotr Zaranek <piotrek.zaranek@gmail.com>
2023-04-19 14:35:17 +0200

    Changed class name

---
+++ BikeForSell.Domain/Models/BikeDetailInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeForSell.Domain.Models
+{
+    public class BikeDetailInformation
+    {
+        public int Id { get; set; }
+        public decimal Prize { get; set; }
+        public bool Delivery { get; set; }
+        public string City { get; set; }
+        public DateTime Date { get; set; }
+
+        // 1:1 DetailInformation To Bike
+        public int BikeRef { get; set; }
+        public Bike Bike { get; set; }
+
+        // 1:N DetalInformation To ApplicationUser
+        public string UserRef { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
